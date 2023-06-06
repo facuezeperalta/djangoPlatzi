e81fffb0629a111e9e77a9fb83c87761af1b7a6b
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-06-06 15:50:56 -0300

    cambios

--- premiosplatzi/premiosplatziapp/polls/views.py
+++ premiosplatzi/premiosplatziapp/polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse #clase que permite ejecutar una respuesta http.
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Hello, world!')
