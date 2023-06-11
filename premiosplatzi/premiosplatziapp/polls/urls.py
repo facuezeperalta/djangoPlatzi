0430c313cc06f1a0dd6ce3ac89787cd7118c5383
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-06-11 00:57:47 -0300

    Vistas creadas

--- premiosplatzi/premiosplatziapp/polls/urls.py
+++ premiosplatzi/premiosplatziapp/polls/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
+
+
 from . import views
 
+
 urlpatterns = [
+    #ex: /poll/
     path('',views.index, name ='index'),
+    #ex: /polls/5/ con esto acceso al detalle de la pregunta 5
+    path('<int:question_id>/',views.detail, name='Vista 01'),
+    #ex: /polls/5/result con esto acceso a los resultados de la pregunta 5
+    path('<int:question_id>/results',views.results, name='Vista 02'),
+    #ex: /polls/5/vote con esto acceso a los votos de la de la pregunta 5
+    path('<int:question_id>/vote',views.vote, name='Vista 03'),
+    #ex: /polls/5/detail
+    path('<int:question_id>/detail',views.detail,name='vista 04')
 ]
--- premiosplatzi/premiosplatziapp/polls/views.py
+++ premiosplatzi/premiosplatziapp/polls/views.py
@@ -2,5 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse #clase que permite ejecutar una respuesta http.
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Hello, world!')
+
+def index(request): #function base view
+    return HttpResponse('Estas en la página principal de Premios Platzi app')
+
+
+def detail(request,question_id):
+    return HttpResponse(f"Estas viendo el detalle de  la pregunta número: {question_id}")
+
+def results(request,question_id):
+    return HttpResponse (f"Estas viendo los resultados de la pregunta número: {question_id}")
+
+def vote(request,question_id):
+    return  HttpResponse(f"Estas votando a la pregunta numero: {question_id}")
+
+
+
