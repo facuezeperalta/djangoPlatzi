34f7a2db816d7dbbb1b69f7c2ebea2384f58c556
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2023-06-11 16:10:39 -0300

    url conDjango

--- premiosplatzi/premiosplatziapp/polls/urls.py
+++ premiosplatzi/premiosplatziapp/polls/urls.py
@@ -3,16 +3,14 @@ from django.urls import path
 
 from . import views
 
-
+app_name = "polls"
 urlpatterns = [
     #ex: /poll/
     path('',views.index, name ='index'),
     #ex: /polls/5/ con esto acceso al detalle de la pregunta 5
-    path('<int:question_id>/',views.detail, name='Vista 01'),
+    path('<int:question_id>/',views.detail, name='detail'),
     #ex: /polls/5/result con esto acceso a los resultados de la pregunta 5
-    path('<int:question_id>/results',views.results, name='Vista 02'),
+    path('<int:question_id>/results',views.results, name='results'),
     #ex: /polls/5/vote con esto acceso a los votos de la de la pregunta 5
-    path('<int:question_id>/vote',views.vote, name='Vista 03'),
-    #ex: /polls/5/detail
-    path('<int:question_id>/detail',views.detail,name='vista 04')
+    path('<int:question_id>/vote',views.vote, name='vote'),
 ]
